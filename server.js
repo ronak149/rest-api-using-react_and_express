42b73d8c043f4179eaee138662d7a24d8f5ea4d3
ronak149 <ronakp6997@gmail.com>
2022-12-29 13:58:28 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+const port = process.env.PORT || 5001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
+app.use("/api", (req,res) => {
+    res.json({ "message": "Hello"});
+});
+
+app.use(express.static(path.join(__dirname, "./frontend/build")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./frontend/build/index.html"));
+});
+
+app.get("/", (req, res) => {
+    res.status(200).send("@ backend");
+});
+
+app.listen(port, () => {
+    console.log(`Server listening on port: ${port}`);
+});
+
+module.exports = app;
